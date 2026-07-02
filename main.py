34217b1059c8731f99b0792261d1ac7b02b7aa80
Jusieee <Smrtv.a@gmail.com>
2026-07-02 18:29:42 +0300

    Создали корзину через списки

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+cart = []
+total_sum = 0
+
 while True:
     work_buying = input('Выберите команду (купить/выход): ')
     if work_buying == "выход":
+        print(f"Вы приобрели {cart}, стоимость товаров вышла :{total_sum} руб.")
         break
-    name = input("Название товара: ")
-    cost = float(input("Цена товара: "))
-    number = int(input("Количество товара: "))
-    summa = cost * number
-    print(f"Добрый день, вы приобрели {name}, стоящий {cost} руб. в в количестве:{number}. Ваша стоимость будет {summa} р.")
-    if summa >= 3000:
-        summa *= 0.9
-        print(f"К вам применяется скидка в 10%, теперь ваша цена составляет - {summa} руб.")
+    elif work_buying == "купить":
+        name = input("Название товара: ")
+        cost = float(input("Цена товара: "))
+        number = int(input("Количество товара: "))
+        cart.append(name)
+        total_sum += cost * number
+        if total_sum >= 3000:
+            total_sum *= 0.9
+            print(f"Товар {name}, добавлен в корзину, вам доступна скидка 10% Ваша итоговая цена сейчас составляет: "
+                f"{total_sum}")
+        else:
+            print(f"Товар {name}, добавлен в корзину, Для получение скидки, наберите корзину на 3000 рублей! Ваша "
+                    f"итоговая цена сейчас составляет: {total_sum}")
     else:
-        print(f"Для скидки нужно набрать от 3000 рублей включительно!")
-
+    print(f"Неизвестная команда, попробуйте еще раз!")
