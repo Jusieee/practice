ba0d0d6acea63b10c383edc5291a62d90a97296b
Jusieee <Smrtv.a@gmail.com>
2026-07-17 23:02:19 +0300

    Добавили эндпойнт POST

--- app.py
+++ app.py
@@ -33,3 +33,64 @@ def get_products():
             "stock": p[3]
         })
     return result
+
+@app.post(
+    "/cart/items",
+    status_code = status.HTTP_201_CREATED,
+)
+def add_to_cart(item: CartItemCreate):
+    connection = sqlite3.connect("online_shop.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    try:
+        cursor.execute(
+            """
+            SELECT id, name, stock
+            FROM product
+            WHERE id = ?
+            """,
+            (item.product_id,)
+        )
+        product = cursor.fetchone()
+
+        # Проверка на наличие товара
+        if product is None:
+            raise HTTPException(
+                status_code = status.HTTP_404_NOT_FOUND,
+                detail = "Товар не найден",
+            )
+
+        # Проверка наличие товара на складе
+        if item.quantity > product["stock"]:
+            raise HTTPException(
+                status_code = status.HTTP_409_CONFLICT,
+                detail = f"Доступно {product["stock"]} шт. товара",
+            )
+
+        # Добавление товара в корзину
+        cursor.execute(
+            """
+            INSERT INTO cart_items (product_id, quantity)
+            VALUE (?, ?)
+            """,
+            (item.product_id, item.quantity),
+        )
+        connection.commit()
+
+        return {
+            "message": "Товар добавлен",
+            "cart_item": {
+                "id": cursor.lastrowid,
+                "product_id": item.product_id,
+                "product_name": product["name"],
+                "quantity": item.quantity,
+            },
+        }
+    except sqlite3.Error:
+        connection.rollback()
+        raise HTTPException(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail = "Ошибка при работе в базе данных"
+        )
+    finally:
+        connection.close()
