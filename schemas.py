eedc1b1fb574fb7036cf1940803527695365eae7
Jusieee <Smrtv.a@gmail.com>
2026-08-07 09:03:46 +0300

    fix

--- schemas.py
+++ schemas.py
@@ -30,7 +30,7 @@ class CartItemCreate(BaseModel):
 class ProductUpdate(BaseModel):
     name: str = Field(
         min_length=1,
-        max_lenght=100,
+        max_length=100,
     )
     price: float = Field(gt=0)
     stock: int = Field(ge=0)
