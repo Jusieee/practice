6ac4467cb0bda3c577168d2f578b596a4f800aac
Jusieee <Smrtv.a@gmail.com>
2026-08-16 15:18:54 +0300

    тест корзины

--- test_services.py
+++ test_services.py
@@ -1,4 +1,29 @@
-def test_example():
-    result = 2 + 2
+from services import get_cart
 
-    assert result == 4
+
+def test_get_cart_calculates_total(monkeypatch):
+    fake_cart_items = [
+        {
+            "id": 1,
+            "product_id": 10,
+            "name": "Мышь",
+            "price": 1000,
+            "quantity": 2
+        },
+        {
+            "id": 2,
+            "product_id": 20,
+            "name": "Клавиатура",
+            "price": 3000,
+            "quantity": 1
+        }
+    ]
+
+    monkeypatch.setattr(
+        "services.get_cart_items",
+        lambda: fake_cart_items
+    )
+
+    result = get_cart()
+
+    assert result["total"] == 5000
