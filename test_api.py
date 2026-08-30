60a6c4b7869b5461723b0162c29164694321ce25
Jusieee <Smrtv.a@gmail.com>
2026-08-30 19:53:06 +0300

    Тест эндпоинта на статус код

--- test_api.py
+++ test_api.py
@@ -2,7 +2,9 @@ from fastapi.testclient import TestClient
 
 from app import app
 
+
 client = TestClient(app)
+
 
 def test_home():
     response = client.get("/")
@@ -12,12 +14,37 @@ def test_home():
         "message": "Добро пожаловать в API нашего интернет-магазина!"
     }
 
+
 def test_get_product_invalid_id():
     response = client.get("/products/0")
 
     assert response.status_code == 422
 
+
 def test_get_product_invalid_string_id():
     response = client.get("/products/hello")
 
     assert response.status_code == 422
+
+def test_get_product_success(monkeypatch):
+    fake_product = {
+        "id": 1,
+        "name": "Мышь",
+        "price": 1000,
+        "stock": 5
+    }
+
+    monkeypatch.setattr(
+        "app.get_product_by_id",
+        lambda product_id: fake_product
+    )
+
+    response = client.get("/products/1")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "Мышь",
+        "price": 1000,
+        "stock": 5
+    }
