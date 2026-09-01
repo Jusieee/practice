e69f6fffe6921f44d12ee19243b60b3bdeb24750
Jusieee <Smrtv.a@gmail.com>
2026-09-01 11:46:08 +0300

    Тест на созданный товар по апи

--- test_api.py
+++ test_api.py
@@ -26,6 +26,7 @@ def test_get_product_invalid_string_id():
 
     assert response.status_code == 422
 
+
 def test_get_product_success(monkeypatch):
     fake_product = {
         "id": 1,
@@ -49,6 +50,7 @@ def test_get_product_success(monkeypatch):
         "stock": 5
     }
 
+
 def test_get_product_not_found(monkeypatch):
     monkeypatch.setattr(
         "app.get_product_by_id",
@@ -60,4 +62,36 @@ def test_get_product_not_found(monkeypatch):
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Товар не найден"
+    }
+
+
+def test_create_product_success(monkeypatch):
+    fake_created_product = {
+        "id": 10,
+        "name": "Монитор",
+        "price": 25000,
+        "stock": 4
+    }
+
+    monkeypatch.setattr(
+        "app.create_product",
+        lambda name, price, stock: fake_created_product
+    )
+
+    response = client.post(
+        "/products",
+        json={
+            "name": "Монитор",
+            "price": 25000,
+            "stock": 4
+        }
+    )
+
+    assert response.status_code == 201
+
+    assert response.json() == {
+        "id": 10,
+        "name": "Монитор",
+        "price": 25000,
+        "stock": 4
     }
