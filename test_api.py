fd55b45317c1548f39743f6fc92b9699c17c1188
Jusieee <Smrtv.a@gmail.com>
2026-09-02 16:39:10 +0300

    Сделал ошибочный тест на удаление товара из корзины - 404

--- test_api.py
+++ test_api.py
@@ -383,7 +383,7 @@ def test_delete_cart_item_not_found(monkeypatch):
     def fake_remove_product_from_cart(*args, **kwargs):
         raise ProductNotFoundError
 
-    response = client.delete("/cart/items/1")
+    response = client.delete("/cart/items/999")
 
     assert response.status_code == 404
 
