78ba8144867bf3c75dc9d0a720c05ea8e14caac5
Jusieee <Smrtv.a@gmail.com>
2026-09-04 18:22:23 +0300

    Создал фейковую базу для тестов

--- conftest.py
+++ conftest.py
@@ -1,5 +1,9 @@
 import pytest
 
+import sqlite3
+
+import database
+
 
 @pytest.fixture
 def fake_product():
@@ -17,3 +21,49 @@ def fake_cart_item():
         "product_id": 1,
         "quantity": 3
     }
+
+@pytest.fixture
+def test_db(monkeypatch, tmp_path):
+    db_path = tmp_path / "test_online_shop.db"
+
+    monkeypatch.setattr(
+        database,
+        "DATABASE_NAME",
+        str(db_path)
+    )
+
+    connection = sqlite3.connect(db_path)
+
+    connection.execute(
+        """
+        CREATE TABLE product(
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            name TEXT NOT NULL
+            price REAL NOT NULL
+            stock INTEGER DEFAULT 0
+            name_key TEXT NOT_NULL UNIQUE
+        )
+        """
+    )
+
+    connection.execute(
+        """
+        CREATE TABLE cart_items(
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            product_id INTEGER
+            quantity INTEGER DEFAULT 0
+        )
+        """
+    )
+
+    connection.execute(
+        """
+        CREATE UNIQUE INDEX idx_cart_items_product_id
+        ON cart_items(product_id)
+        """
+    )
+
+    connection.commit()
+    connection.close()
+
+    yield db_path
