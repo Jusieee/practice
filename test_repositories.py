498dab6b7be40d841433e55db125c6be53bbe0d3
Jusieee <Smrtv.a@gmail.com>
2026-09-04 22:11:31 +0300

    Тест репозитория на создание дубликата name_key

--- test_repositories.py
+++ test_repositories.py
@@ -3,6 +3,11 @@ from repositories import (
     get_product_by_id
 )
 
+import pytest
+
+import sqlite3
+
+
 def test_create_and_get_product(test_db):
     created_product = create_product(
         name="Монитор",
@@ -16,6 +21,28 @@ def test_create_and_get_product(test_db):
 
     assert product is not None
     assert product["id"] == created_product["id"]
+    assert product["name"] == "Монитор"
+    assert product["price"] == 25000
+    assert product["stock"] == 4
+
+
+def test_create_product_dublicate_name(test_db):
+    first_product = create_product(
+        name="Монитор",
+        price=25000,
+        stock=4
+    )
+
+    with pytest.raises(sqlite3.IntegrityError):
+        create_product(
+            name=" МоНиТоР ",
+            price=30000,
+            stock=10
+        )
+
+    product = get_product_by_id(first_product["id"])
+
+    assert product is not None
     assert product["name"] == "Монитор"
     assert product["price"] == 25000
     assert product["stock"] == 4
